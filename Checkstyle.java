3f1c553c15b96704e59e064642cde462faebb4c5
TheIAceI <rcaldas46@gmail.com>
2016-12-08 14:57:26 +0000

    Create Checkstyle.java

---
+++ Checkstyle.java
@@ -0,0 +1,18 @@
+public class Checkstyle {
+	
+	static public void teste(){
+		int a = 2;
+		int b = 3;
+		
+		if(a == b){
+			System.out.println("Oooops");
+		} else
+			System.out.println("ok");
+	}
+  
+  
+	
+	public static void main(String[] args){
+		teste();
+	}
+}
